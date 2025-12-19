5b1d8bde3256540735c94bf06b673dec910daeb6
Suniti Paliwal <sunitipaliwal99@gmail.com>
2025-12-20 01:03:05 +0530

    d-9 q-1

---
+++ L-542.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n= mat.size();
+        int m= mat[0].size();
+        vector<vector<int>> vis(n , vector<int>(m,0));
+        vector<vector<int>>  dist(n, vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+            if(mat[i][j]==0){
+                q.push({{i,j},0});
+                vis[i][j]=1;
+            }
+            else{
+                vis[i][j]=0;
+            }
+          }
+        }
+
+        int delR[]= {-1,0,1,0};
+        int delC[]= {0,1,0,-1};
+        while(!q.empty()){
+            int r= q.front().first.first;
+            int c= q.front().first.second;
+            int steps= q.front().second;
+            q.pop();
+            dist[r][c]= steps;
+
+            for(int i=0;i<4;i++){
+                int nr= r+delR[i];
+                int nc= c+delC[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0){
+                    vis[nr][nc]=1;
+                    q.push({{nr,nc},steps+1});
+                }
+            }
+        }
+        return dist;
+    }
+};
